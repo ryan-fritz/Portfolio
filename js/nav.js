673ce44a96940a724a8f932c38fb925156f593ca
FritzTechNeuro <rfritz002@gmail.com>
2024-07-28 11:58:01 -0400

    test

--- js/nav.js
+++
@@ -1,8 +0,0 @@
-// nav.js
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("nav.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("nav-placeholder").innerHTML = data;
-        });
-});
