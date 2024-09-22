1c72dbc6c90874adeb22e3c2d0fca0ac15cfd0e9
FritzTechNeuro <rfritz002@gmail.com>
2024-09-21 21:53:32 -0400

    refactor: simplify dark mode toggle script and improve readability

--- js/dark-mode-toggle.js
+++ js/dark-mode-toggle.js
@@ -1,23 +1,22 @@
-// Dark Mode Toggle Script
+// dark-mode-toggle.js
 document.addEventListener('DOMContentLoaded', function () {
     const toggleSwitch = document.getElementById('darkModeToggle');
     const currentTheme = localStorage.getItem('theme');
-
-    // Check if the stored theme is 'dark', then apply dark mode and check the switch
-    
+  
     if (currentTheme === 'dark') {
-        document.body.classList.add('dark-mode');
-        toggleSwitch.checked = true;
+      document.body.classList.add('dark-mode');
+      if (toggleSwitch) toggleSwitch.checked = true;
     }
-
-    // Add event listener to the toggle switch to change the theme
-    toggleSwitch.addEventListener('change', function () {
+  
+    if (toggleSwitch) {
+      toggleSwitch.addEventListener('change', function () {
         document.body.classList.toggle('dark-mode');
         let theme = 'light';
         if (document.body.classList.contains('dark-mode')) {
-            theme = 'dark';
+          theme = 'dark';
         }
         localStorage.setItem('theme', theme);
-    });
-    
-});
+      });
+    }
+  });
+  
